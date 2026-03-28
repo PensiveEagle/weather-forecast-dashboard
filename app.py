4077c5734f7f96dee900b21efdce32eecf115f82
PensiveEagle <halljf96@gmail.com>
2026-03-28 08:55:24 +0000

    Added hard-coded chart to interface

--- app.py
+++ app.py
@@ -1,7 +1,14 @@
 import streamlit as st
+import plotly.express as px
 
 st.title( f"Weather forecast for the next days" )
 location = st.text_input( "Location: " )
 number_of_days = st.slider( "Forecasted days: ", min_value = 1, max_value = 5 )
 data_view = st.selectbox( "Select data to view: ", ( "Temperature", "Sky" ) )
 st.subheader( f"{data_view} for the next {number_of_days} days in {location}" )
+
+
+dates = ["2022-10-25", "2022-10-26", "2022-10-27"]
+temperatures = ["23", "09", "15"]
+chart = px.line( x = dates, y = temperatures, labels = {"x": "Date", "y": "Temperature (C)"})
+st.plotly_chart( chart )
