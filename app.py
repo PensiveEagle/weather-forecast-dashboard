53052863d76d395a5d9113ae76498bb6c0654e40
PensiveEagle <halljf96@gmail.com>
2026-04-06 14:37:43 +0100

    Set display to change based on data view selection

--- app.py
+++ app.py
@@ -11,12 +11,19 @@ if location == "":
 number_of_days = st.slider( "Forecasted days: ", min_value = 1, max_value = 5 )
 data_view = st.selectbox( "Select data to view: ", ( "Temperature", "Sky" ) )
 
-st.subheader( f"{data_view} for the next {number_of_days} days in {location}" )
+if data_view == "Temperature":
+    st.subheader( f"{data_view} for the next {number_of_days} days in {location}" )
 
-data = backend.get_data( location = location )
+    data = backend.get_data( location = location )
 
-time, temperatures = backend.extract_time_and_temps( data = data, forecast_days = number_of_days )
+    time, temperatures = backend.extract_time_and_temps( data = data, forecast_days = number_of_days )
 
-chart = backend.generate_chart( x_data = time, y_data = temperatures, x_label = "Time", y_label = "Temperature (C)")
+    chart = backend.generate_chart( x_data = time, y_data = temperatures, x_label = "Time", y_label = "Temperature (C)")
 
-st.plotly_chart( chart )
+    st.plotly_chart( chart )
+    
+elif data_view == "Sky":
+    st.subheader( "Feature Coming Soon..." )
+    
+else:
+    st.subheader( "Application failure!" )
